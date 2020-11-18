c3e5a6df22ed23993953a6e4d91a8153bdbbd0ea
Crystal K Young <maybiz@hotmail.com>
2020-11-18 12:15:28 -0500

    remove task

--- main.js
+++ main.js
@@ -44,8 +44,8 @@ const handleMenu = function(cmd) {
     interface.question('What todo would you like to add?\n\n', add);
   } else if (cmd === '2') {
     console.clear();
-    console.log(`Feature ${cmd} is still under construction. Sorry!`);
-    interface.question(menu, handleMenu);
+    numberedList()
+    interface.question('\nWhich todo do you want to remove?\n', remove)
   } else if (cmd === '3') {
     console.clear();
     console.log(`Feature ${cmd} is still under construction. Sorry!`);
@@ -66,8 +66,44 @@ const handleMenu = function(cmd) {
 
 console.clear();
 console.log('Your todos are:')
-for (const todo of todos) {
-  console.log('* ' + todo.text);
+
+
+const numberedList = function() {
+for (let i = 0; i < todos.length; i++) {
+  let toDo = ''
+  const singleToDo = todos[i]
+  const text = singleToDo.text
+  const num = i + 1
+toDo = toDo + num + ' --- ' + text
+console.log(toDo)
+  }
+}
+numberedList()
+
+
+const remove = function(num) {
+    if (cmd === '1') {
+        interface.question('\nList your tasks', add)
+    } else if (cmd === '2') {
+        displayToDos(todos);
+        interface.question('\nRemove a task', remove)
+    } else if (cmd === '3') {
+        displaysToDos(todos);
+        interface.question('\nMark complete', complete)
+    } 
+}
+console.clear()
+
+const displayMenu = function() {
+numberedList()
 }
 
-interface.question(menu, handleMenu);
+
+interface.question(menu, handleMenu)
+
+
+// * Now all we have to do is `splice` the item at that location in the array. What you want to research for this one is how to use `splice` to remove one item only at a certain index. And remember! They're not actually passing you an index... they're passing you a _number_, which means your 0-based counting will be off by 1.
+// * Then, of course, we'll need to show the user their new todo list, with the todo they picked `splice`d right out. And display the menu again, looping them back to your menu-handling function.
+
+
+
